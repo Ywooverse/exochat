10dac4661bba00de1cade4dedfc938f0e5fdf38a
Ywooverse <conkon85@gmail.com>
2024-07-29 14:50:49 +0900

    error fix

--- app.py
+++ app.py
@@ -3,11 +3,6 @@ import streamlit as st
 from openai import OpenAI
 import time
 
-secret = st.secrets["api_keys"]["secret"]
-
-# Streamlit Cloud에서 환경 변수 불러오기
-API_KEY = os.getenv(secret)
-
 # OpenAI 클라이언트 설정
 client = OpenAI(api_key=API_KEY)
 
